96bc9b50bc63e9c3d020b5adaa4dace11d6505e0
HousseinT <talebh20002@gmail.com>
2025-05-28 03:15:15 +0300

    edit AuthLayout file

--- Authentication-Page/src/components/auth/RegisterForm.tsx
+++ Authentication-Page/src/components/auth/RegisterForm.tsx
@@ -133,43 +133,26 @@ const RegisterForm: React.FC = () => {
         </Button>
       </div>
       
-      <div className="pt-3">
+      <div className="pt-5">
         <div className="relative flex items-center">
           <div className="border-t border-gray-300 flex-grow"></div>
           <div className="px-4 text-sm text-gray-500 font-medium">or sign up with</div>
           <div className="border-t border-gray-300 flex-grow"></div>
         </div>
         
-        <div className="grid grid-cols-3 gap-3 mt-4">
+        <div className="flex justify-center mt-5">
           <button
             type="button"
-            className="flex items-center justify-center w-full px-4 py-2.5 border border-gray-300 rounded-xl shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-0 transition-all duration-200 hover:shadow-md"
+            className="flex items-center justify-center px-6 py-3 border border-gray-300 rounded-xl shadow-sm bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-0 transition-all duration-200 hover:shadow-md w-64 mx-auto"
+            onClick={() => alert('Google login coming soon!')}
           >
-            <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="w-6 h-6 mr-3" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M22.56 12.25C22.56 11.47 22.49 10.72 22.36 10H12V14.26H17.92C17.66 15.63 16.88 16.79 15.71 17.57V20.34H19.28C21.36 18.42 22.56 15.6 22.56 12.25Z" fill="#4285F4"/>
               <path d="M12 23C14.97 23 17.46 22.02 19.28 20.34L15.71 17.57C14.73 18.23 13.48 18.63 12 18.63C9.19 18.63 6.8 16.73 5.95 14.1H2.27V16.94C4.08 20.47 7.76 23 12 23Z" fill="#34A853"/>
               <path d="M5.95 14.1C5.75 13.47 5.63 12.79 5.63 12.09C5.63 11.39 5.75 10.71 5.95 10.08V7.24H2.27C1.46 8.68 1 10.35 1 12.09C1 13.83 1.46 15.5 2.27 16.94L5.95 14.1Z" fill="#FBBC05"/>
               <path d="M12 5.55C13.57 5.55 14.97 6.08 16.09 7.15L19.28 3.96C17.46 2.26 14.97 1.27 12 1.27C7.76 1.27 4.08 3.8 2.27 7.33L5.95 10.17C6.8 7.54 9.19 5.55 12 5.55Z" fill="#EA4335"/>
             </svg>
-            Google
-          </button>
-          <button
-            type="button"
-            className="flex items-center justify-center w-full px-4 py-2.5 border border-gray-300 rounded-xl shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-0 transition-all duration-200 hover:shadow-md"
-          >
-            <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M24 12.073C24 5.446 18.627 0.073 12 0.073C5.373 0.073 0 5.446 0 12.073C0 18.063 4.388 23.027 10.125 23.927V15.542H7.078V12.072H10.125V9.43C10.125 6.423 11.917 4.761 14.658 4.761C15.97 4.761 17.344 4.996 17.344 4.996V7.949H15.83C14.34 7.949 13.874 8.874 13.874 9.823V12.073H17.202L16.67 15.543H13.874V23.928C19.612 23.027 24 18.062 24 12.073Z" fill="#1877F2"/>
-            </svg>
-            Facebook
-          </button>
-          <button
-            type="button"
-            className="flex items-center justify-center w-full px-4 py-2.5 border border-gray-300 rounded-xl shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-0 transition-all duration-200 hover:shadow-md"
-          >
-            <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 0C5.373 0 0 5.373 0 12C0 17.303 3.438 21.8 8.207 23.387C8.807 23.498 9.027 23.126 9.027 22.81C9.027 22.524 9.016 21.77 9.01 20.767C5.672 21.492 4.968 19.158 4.968 19.158C4.422 17.773 3.633 17.403 3.633 17.403C2.546 16.659 3.717 16.675 3.717 16.675C4.922 16.759 5.556 17.912 5.556 17.912C6.626 19.746 8.363 19.216 9.05 18.909C9.158 18.134 9.467 17.604 9.81 17.305C7.145 17.004 4.344 15.971 4.344 11.374C4.344 10.063 4.812 8.993 5.579 8.153C5.455 7.85 5.044 6.629 5.696 4.977C5.696 4.977 6.704 4.655 8.997 6.207C9.954 5.941 10.98 5.808 12 5.803C13.02 5.808 14.047 5.941 15.006 6.207C17.297 4.655 18.303 4.977 18.303 4.977C18.956 6.63 18.545 7.851 18.421 8.153C19.191 8.993 19.656 10.064 19.656 11.374C19.656 15.983 16.849 16.998 14.177 17.295C14.607 17.667 14.992 18.397 14.992 19.517C14.992 21.119 14.976 22.409 14.976 22.81C14.976 23.129 15.192 23.504 15.8 23.386C20.566 21.797 24 17.3 24 12C24 5.373 18.627 0 12 0Z" fill="#333333"/>
-            </svg>
-            GitHub
+             Google
           </button>
         </div>
       </div>
